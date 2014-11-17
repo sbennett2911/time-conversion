39dbec1c8ce2f247812b3a643448c95631105bc4
Steven Bennett <steven.bennett2911@icloud.com>
2014-11-16 20:22:19 -0800

    Initial Commit
    built Time class and tested in a console app.

---
+++ TimeConversion/TimeConversion/Program.cs
@@ -0,0 +1,36 @@
+﻿/*This application is intended to convert time from hh:mm:ss to total seconds.
+ *User will input hours, minutes, and seconds.  
+ *The time will be converted to seconds and displayed.
+ *The purpose is to lay the foundatio for a pace calculator.*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimeConversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Construct new Time object.
+            Time time = new Time();
+
+            //Get user input for hours, minutes, and seconds.
+            Console.Write("Enter hours: ");
+            time.Hours = double.Parse(Console.ReadLine()); //Probably need to use TryParse which returns a bool for exception handling purposes.
+
+            Console.Write("Enter minutes: ");
+            time.Minutes = double.Parse(Console.ReadLine()); //Need to add exception handling.
+
+            Console.Write("Enter seconds: ");
+            time.Seconds = double.Parse(Console.ReadLine()); //Need to add exception handling.
+
+            Console.Write("Total seconds: ");
+            Console.WriteLine(time.ToSeconds(time.Hours, time.Minutes, time.Seconds));
+            
+        }
+    }
+}
---
+++ TimeConversion/TimeConversion/Time.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimeConversion
+{
+    class Time
+    {
+        //properties
+        public double Hours { get; set; }
+        public double Minutes { get; set; }
+        public double Seconds { get; set; }
+
+        //methods
+        public double ToSeconds(double h, double m, double s)
+        {
+            double totalSeconds = 0.0;
+            double convertedHours = 0.0;
+            double convertedMinutes = 0.0;
+            
+            //convert hours to seconds
+            convertedHours = h * 3600;
+
+            //convert minutes to seconds
+            convertedMinutes = m * 60;
+
+            //add seconds together from input to find total seconds
+            totalSeconds = convertedHours + convertedMinutes + s;
+
+            return totalSeconds;
+        }
+    }
+}
